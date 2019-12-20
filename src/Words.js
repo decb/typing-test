f9ac1c390b68d4780a7607a04e94bb10de64501e
decb <decb@users.noreply.github.com>
2019-12-20 12:18:29 +0000

    Improved styling and layout a little

--- src/Words.js
+++ src/Words.js
@@ -5,23 +5,24 @@ function Words(props) {
   const [lower, upper] = range;
   const first = words.slice(lower)[0];
   const firstStyle = {
+    padding: "3px",
+    fontWeight: "bold",
     backgroundColor:
       first === progress
         ? "#b7eb8f"
         : first.indexOf(progress) === 0
-          ? "#ffe58f"
-          : "#ffa39e"
+        ? "#ffe58f"
+        : "#ffa39e"
   };
 
-  const visible = props.words
-    .slice(lower + 1, upper)
-    .join(" ")
-    .concat([" ..."]);
+  const visible = props.words.slice(lower + 1, upper).join(" ");
 
   return (
-    <code>
-      <span style={firstStyle}>{first}</span> {visible}
-    </code>
+    <div style={{ whiteSpace: "nowrap", overflow: "hidden" }}>
+      <code>
+        <span style={firstStyle}>{first}</span> {visible}
+      </code>
+    </div>
   );
 }
 
